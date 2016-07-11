18421ade2709cce485a140dd3d9121d4fb499303
Stu Porter <sporter21@gmail.com>
2016-07-11 13:07:41 -0500

    Before lunch break

---
+++ LawyerUp/src/controllers/home.js
@@ -0,0 +1,15 @@
+var express         = require('express'),
+    HomeController  = express.Router(),
+    User            = require(__dirname + '/../models/user'),
+    bcrypt          = require('bcrypt');
+
+    HomeController.route('/?')
+  // GET /
+  // -----
+  // Serve the homepage
+  .get(function(req, res, next) {
+    res.render('home', {});
+  })
+
+
+module.exports = HomeController;
