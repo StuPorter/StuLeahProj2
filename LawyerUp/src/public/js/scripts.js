9df6777052e6d2859c9702e44cfce1a7258d3bc9
LeahNelson <webdevleah@gmail.com>
2016-07-14 15:00:13 -0500

    Final commit before final maerge profmade now showing

--- LawyerUp/src/public/js/scripts.js
+++ LawyerUp/src/public/js/scripts.js
@@ -2,6 +2,8 @@
 //   event.preventDefault();
 // });
 
-$("#logout-btn").on('click', function(event) {
-  req.session.isLoggedIn= false;
+$('#logout-btn').on('click', function(event) {
+  // req.session.isLoggedIn = false;
+  window.location = '/?';
+
 })
